a6b9d93c6f053325d7e0031365e59daf1ad95ec2
Waqar Ali <wali@ku.edu>
2020-11-07 11:44:45 -0600

    setup: one time packages to install in a new linux distro

---
+++ xavier/one_time_packages.sh
@@ -0,0 +1,24 @@
+R='\033[0;31m'
+G='\033[0;32m'
+Y='\033[0;33m'
+C='\033[0;36m'
+N='\033[0m'
+
+if [ "`whoami`" != "root" ]; then
+	echo -e "${R}[ERROR] Only root can run this script!${N}"
+	return
+fi
+
+packages=("vim" "tmux" "tree" "htop" "nmon" "trace-cmd" "kernelshark" "cscope" "ctags")
+
+echo -e "${G}Updating APT...${N}"
+apt update
+
+for p in ${packages[@]}; do
+	echo
+	echo -e "${G}[STATUS] Installing: ${p}${N}"
+	apt install -y ${p}
+done
+
+echo
+echo -e "${Y}[STATUS] Installation complete!${N}"
